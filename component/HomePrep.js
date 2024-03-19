886458644179b3471684e3242343074f368b46b0
Test <lucas.almeida.81100@gmail.com>
2024-03-19 16:41:49 +0100

    V0.0.3

--- component/HomePrep.js
+++ component/HomePrep.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
 import axios from 'axios';
 
 const HomePrep = () => {
@@ -9,7 +10,6 @@ const HomePrep = () => {
     const fetchData = async () => {
       try {
         const response = await axios.get('https://automator.benazouz-ouilem.com/webhook/data');
-
         setData(response.data);
       } catch (error) {
         console.error('Erreur lors de la récupération des données de l\'API :', error);
@@ -21,13 +21,31 @@ const HomePrep = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.label}>Données de l'API :</Text>
       {data ? (
-        <View style={styles.dataContainer}>
-          <Text>Batterie : {data[0].body.battery}</Text>
-          <Text>Gravité : {data[0].body.gravity}</Text>
-          <Text>Température : {data[0].body.temperature}</Text>
-          <Text>Inclinaison : {data[0].body.tilt}</Text>
+        <View style={styles.card}>
+          {/* Vue pour l'icône de la cuve */}
+          <View style={styles.iconContainer}>
+            <FontAwesome5 name="beer" size={60} color="#fff" />
+          </View>
+          {/* Vue pour les données */}
+          <View style={styles.dataContainer}>
+            <View style={styles.cardContent}>
+              <Text style={styles.cardLabel}>Batterie :</Text>
+              <Text style={styles.cardText}>{data[0].body.battery}</Text>
+            </View>
+            <View style={styles.cardContent}>
+              <Text style={styles.cardLabel}>Gravité :</Text>
+              <Text style={styles.cardText}>{data[0].body.gravity}</Text>
+            </View>
+            <View style={styles.cardContent}>
+              <Text style={styles.cardLabel}>Température :</Text>
+              <Text style={styles.cardText}>{data[0].body.temperature}</Text>
+            </View>
+            <View style={styles.cardContent}>
+              <Text style={styles.cardLabel}>Inclinaison :</Text>
+              <Text style={styles.cardText}>{data[0].body.tilt}</Text>
+            </View>
+          </View>
         </View>
       ) : (
         <Text>Chargement des données...</Text>
@@ -37,19 +55,47 @@ const HomePrep = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  label: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  dataContainer: {
-    marginTop: 10,
-  },
-});
-
-export default HomePrep;
+    container: {
+      flex: 1,
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      marginTop: 180,
+    },
+    card: {
+      backgroundColor: '#E8D038',
+      borderRadius: 20,
+      padding: 20,
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+      elevation: 5,
+      flexDirection: 'row',
+      width: '145%'
+    },
+    iconContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    dataContainer: {
+      flex: 2,
+      paddingLeft: 10,
+    },
+    cardContent: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginBottom: 10,
+    },
+    cardLabel: {
+      fontWeight: 'bold',
+    },
+    cardText: {
+      marginLeft: 10,
+    },
+  });
+  
+  export default HomePrep;
